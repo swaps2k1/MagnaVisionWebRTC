cb38ffc583a9d997d507fd9def10f52646a1269a
swaps2k1 <swaps_2k1@yahoo.com>
2014-04-21 03:16:55 +0530

    Info View Controller update

---
+++ MagnaVisionWebRTC/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  MagnaVisionWebRTC
+//
+//  Created by swapnil on 21/04/14.
+//  Copyright (c) 2014 Simcoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
--- MagnaVisionWebRTC/ViewController.m
+++ MagnaVisionWebRTC/ViewController.m
@@ -20,6 +20,11 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)viewWillAppear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
